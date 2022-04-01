31ed4cb8d780f087fc5fc1248c6fe9762e200565
Sfzmango <maunghtike1999@gmail.com>
2022-03-31 22:00:28 -0700

    add comments, deleted c.logs, consolidated js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 // Query button press 
 var generateBtn = document.querySelector("#generate");
-var passwordLength;
+var passwordBoxText = document.querySelector("#password");
 
+// Add event listener to generate button
+// Listens for the click on generateBtn (generate password button) and runs the function writePassword.
+generateBtn.addEventListener("click", writePassword);
+
+var passwordLength;
 var confirmArr = [false, false, false, false];
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,8 +27,8 @@ function userInputLength() {
     alert("Please input a value between 8 and 128.")
     userInputLength();
     return userInputLength;
-  }
-}
+  };
+};
 
 //confirms with user if they would like to add lowercase, uppercase, numbers, and/or special characters.
 function userInputConfirm() {
@@ -33,28 +38,33 @@ function userInputConfirm() {
   confirmArr[3] = window.confirm("Would you like to include special characters?");
 };
 
+// filters out any falsy vaulues
+function filterX(x) {
+  return x.filter(Boolean);
+};
+
 //turns all confirmed options into a single string.
 function confirmedChar() {
   for (let i = 0; i < 4; i++) {
     if (confirmArr[0] == true) {
       confirmArr[0] = lowercaseLetters;
-    }
+    };
     if (confirmArr[1] == true) {
       confirmArr[1] = uppercaseLetters;
-    }
+    };
     if (confirmArr[2] == true) {
       confirmArr[2] = numbers;
-    }
+    };
     if (confirmArr[3] == true) {
       confirmArr[3] = specialCharacters;
-    }
-    // filters out all falsy values from array.
-    function filterArr(arr) {
-      return arr.filter(Boolean);
     };
-    var filteredArr = filterArr(confirmArr);
+
+    // filters out all falsy values from array.
+    var filteredArr = filterX(confirmArr);
+
     // joins all index values from the filtered array together into one big string.
     confirmedCharacters = filteredArr.join("");
+
     // updates confirmedCharactersLength to the number of the length of the string.
     confirmedCharactersLength = confirmedCharacters.length;
   };
@@ -73,45 +83,20 @@ function writePassword() {
 
   userInputLength();
 
-  // checks if the user hit cancel on the first prompt.
-  if (passwordLength !== null) {
+  // checks if the prompt gave back a truthy value (user hit cancel on the first prompt.)
+  // if truthy value exist, continue on with the code.
+  if (passwordLength) {
     userInputConfirm();
     confirmedChar();
     generatePassword();
+
+    // sets the box text on the html id for #password to the randomly generated password.
+    passwordBoxText.textContent = generatedPassword;
+
+    // resets the generated password for multiple reuse without refreshing the page.
+    generatedPassword = "";
+  }
+  else {
+    return;
   };
-
-  console.log(passwordLength);
-  console.log(confirmArr);
-  console.log(confirmedCharacters);
-  console.log(confirmedCharactersLength);
-  console.log(generatedPassword);
-
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
 };
-
-
-
-// const str = "abcdefg";
-// const strlength = 7;
-// result = "";
-// function randompass(passwordLength) {
-//   for (let i = 0; i < passwordLength; i++) {
-//     var rng = (Math.floor(Math.random() * strlength))
-//     console.log(rng);
-//     result += str.substring(rng, rng + 1);
-//   }
-//   console.log(result);
-//   return result;
-// };
-
-// randompass(10)
-
-// Add event listener to generate button
-// Listens for the click on generateBtn (generate password button) and runs the function writePassword.
-generateBtn.addEventListener("click", writePassword);
-
-// 
